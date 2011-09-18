2c78928e1d22ad8f066662906e0f39d2c83a4648
Mick Thompson <dthompson@gmail.com>
2011-09-18 15:52:09 -0600

    Initial Commit

---
+++ CouchbaseMobileTableView/ModelController.h
@@ -0,0 +1,16 @@
+//
+//  ModelController.h
+//  CouchbaseMobileTableView
+//
+//  Created by Mick Thompson on 9/18/11.
+//  Copyright (c) 2011 DavidMichaelThompson.com. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@class DataViewController;
+
+@interface ModelController : NSObject <UIPageViewControllerDataSource>
+- (DataViewController *)viewControllerAtIndex:(NSUInteger)index;
+- (NSUInteger)indexOfViewController:(DataViewController *)viewController;
+@end
